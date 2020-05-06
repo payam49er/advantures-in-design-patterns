a28aa29efbaa96b1115e1c611584dcc4105824c2
Payam Shoghi <payam49er@gmail.com>
2020-05-06 18:09:50 -0400

    builder test

---
+++ BuilderReview/BuilderReview/CodeBuilder.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace BuilderReview
+{
+ 
+    public class CodeBuilder 
+    {
+        private Dictionary<string, string> properties = new Dictionary<string, string>();
+        private string className;
+        private readonly string indent = new string(' ', 4);
+
+
+        StringBuilder sb = new StringBuilder();
+
+        public CodeBuilder ( string className )
+        {
+            this.className = className;
+        }
+
+        public override string ToString()
+        {
+            sb.AppendLine($"public class {className}");
+            sb.AppendLine("{");
+            foreach (KeyValuePair<string, string> property in properties)
+            {
+                sb.AppendLine($"{indent}public {property.Value} {property.Key};");
+            }
+            sb.AppendLine("}");
+
+            return sb.ToString();
+        }
+        public CodeBuilder AddField ( string fieldName, string typeName )
+        {
+            if (!properties.ContainsKey(fieldName))
+            {
+                properties.Add(fieldName, typeName);
+            }
+
+            return this;
+        }
+    }
+
+
+
+}
---
+++ BuilderReview/BuilderReview/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BuilderReview
+{
+    class Program
+    {
+        static void Main ( string[] args )
+        {
+            var cb = new CodeBuilder("Person").AddField("Name", "string").AddField("Age", "int");
+            Console.WriteLine(cb);
+        }
+    }
+}
