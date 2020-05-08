8fd9077c0092e0f725e8fb9d172e355b7959eb51
Payam Shoghi <payam49er@gmail.com>
2020-05-08 14:43:13 -0400

    Singleton test

---
+++ SingletonTest/SingletonTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SingletonTest
+{
+
+    public class SingletonTester
+    {
+        public static bool IsSingleton(Func<object> func)
+        {
+            var a = func.Invoke();
+            var b = func.Invoke();
+            return ReferenceEquals(a, b);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+
