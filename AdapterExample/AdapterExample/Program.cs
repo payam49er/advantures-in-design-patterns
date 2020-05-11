662118c6b12f0dc863c7233efe2ef7ad8b8c427b
Payam Shoghi <payam49er@gmail.com>
2020-05-11 17:37:53 -0400

    Added adapter example

---
+++ AdapterExample/AdapterExample/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace AdapterExample
+{
+    public class Square
+    {
+        public int Side;
+    }
+
+    public interface IRectangle
+    {
+        int Width { get; }
+        int Height { get; }
+    }
+
+    public static class ExtensionMethods
+    {
+        public static int Area(this IRectangle rc)
+        {
+            return rc.Width * rc.Height;
+        }
+    }
+
+    public class SquareToRectangleAdapter : IRectangle
+    {
+        public SquareToRectangleAdapter(Square square)
+        {
+            // todo
+            this.Width = square.Side;
+            this.Height = square.Side;
+        }
+
+        // todo
+        public int Width { get; }
+        public int Height { get; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Square square = new Square();
+            square.Side = 5;
+            var sqToRectangle =  new SquareToRectangleAdapter(square);
+            var area = sqToRectangle.Area();
+            Console.WriteLine(area);
+
+        }
+    }
+}
