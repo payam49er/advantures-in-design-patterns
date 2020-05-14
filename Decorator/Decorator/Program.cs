febaada70c235afe5cec352b5b135c79320aaded
Payam Shoghi <payam49er@gmail.com>
2020-05-14 01:13:21 -0400

    added decorator

---
+++ Decorator/Decorator/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Decorator
+{
+
+   
+    class Program
+    {
+        static void Main ( string[] args )
+        {
+            //var cb = new CodeBuilder();
+            //cb.AppendLine("class Foo")
+            //    .AppendLine("{")
+            //    .AppendLine("}");
+            //Console.Write(cb);
+
+            //MyStringBuilder s = "Hello";
+            //s += " world";
+            //Console.WriteLine(s);
+
+            //var d = new Dragon();
+            //d.Weight = 120;
+            //d.Fly();
+            //d.Crawl();
+
+            //AnotherDragon anotherDragon = new AnotherDragon {Age = 12};
+
+            //pay attention to this casting! 
+            //((IAnotherBird)anotherDragon).Fly(); 
+
+            //if(anotherDragon is IAnotherBird bird)
+            //    bird.Fly();
+
+            //if(anotherDragon is IAnotherLizard lizard)
+            //    lizard.Crawl();
+
+            var square = new Square(5.6f);
+            Console.WriteLine(square.AsString());
+
+            var redSquare = new ColoredShape(square,"red");
+            Console.WriteLine(redSquare.AsString());
+
+            var transparentSquare = new TransparentShape(redSquare,0.5f);
+            Console.WriteLine(transparentSquare.AsString());
+        }
+    }
+}
---
+++ Decorator/Decorator/Shape.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Decorator
+{
+    public interface IShape
+    {
+        string AsString();
+    }
+
+    public class Circle:IShape
+    {
+        private float radius;
+
+        public Circle(float radius)
+        {
+            this.radius = radius;
+        }
+
+        public void Resize(float factor)
+        {
+            radius *= factor;
+        }
+
+        public string AsString() => $"A circile with radius {radius}";
+    }
+
+
+    public class Square:IShape
+    {
+        private float side;
+
+        public Square(float side)
+        {
+            this.side = side;
+        }
+        public string AsString() => $"A square with side of {side}";
+    }
+
+    public class ColoredShape:IShape
+    {
+        private IShape shape;
+        private string color;
+
+        public ColoredShape(IShape shape, string color)
+        {
+            this.shape = shape;
+            this.color = color;
+        }
+
+        public string AsString() => $"{shape.AsString()} has the color of {color}";
+
+    }
+
+
+    public class TransparentShape:IShape
+    {
+        private IShape shape;
+        private float transparency;
+
+        public TransparentShape(IShape shape, float transparency)
+        {
+            this.shape = shape;
+            this.transparency = transparency;
+        }
+        public string AsString() => $"{shape.AsString()} has the transparency of {transparency * 100}";
+    }
+
+}
